6ca40df94291169c10b2a50f94b9a25b82e1a2cb
Manishkumar Makhija <43664088+manx151089@users.noreply.github.com>
2023-03-14 18:15:26 +0000

    adding setProject shelf feature

--- python/setShowBasedEnv/setHscriptEnvirons.py
+++ python/setShowBasedEnv/setHscriptEnvirons.py
@@ -1,23 +1,45 @@
 import os 
 import hou
+import platform
 def setShowHou():
     projname = hou.ui.readInput('enter show code')[1]
-    
-    path=('/media/vis/sg/prod/'+projname+'/')
+    studioProdDir = 'D:/prod/'
+    path=(studioProdDir+projname+'/')
     if os.path.exists(path):
         os.environ['JOB'] = path
+    else:
+        print('Proj:',path,'\ndoes not exist')
+        if hou.ui.displayConfirmation("Create folder?", suppress=hou.confirmType.OverwriteFile):
+            os.makedirs(path,exist_ok=True)
+            if os.path.exists(path):
+                os.environ['JOB'] = path
+            else:
+                print('permissionDenied: Could not create Job')
         
     print (os.environ['JOB'])
 
 def setSeqHou():
     job = hou.getenv('JOB')
     seqsDir = os.path.join(job,'shots')
-    if os.path.exists(seqsDir):
+    print('...',seqsDir,'exists:',os.path.exists(seqsDir))
+    if os.path.exists(seqsDir) and len(os.listdir(seqsDir))!=0:
         seqs = os.listdir(seqsDir)
         seqSel=hou.ui.selectFromList(seqs,title='setSequence')
         project = os.path.join(seqsDir,seqs[seqSel[0]])
+        if(platform.system()=="Windows"):
+            project = project.replace("\\","/")
+            
+        print('setting SEQ env to',project)
         hou.hscript("set -g SEQ = {}".format(project))
-        print('set seq to:',hou.getenv('SEQ'))
+        #print('set seq to:',hou.getenv('SEQ'))
+
+    else:
+        print('Seq:',seqsDir,'\ndoes not exist')
+        if hou.ui.displayConfirmation("Create folder?", suppress=hou.confirmType.OverwriteFile):
+            input_seqname = hou.ui.readInput('input sequence name')
+            dir_path = os.path.join(seqsDir,input_seqname[1])
+            print(dir_path)
+            os.makedirs(dir_path,exist_ok=True)
 
 def setShotHou():
     import hou
@@ -26,10 +48,25 @@ def setShotHou():
     shotsDir = seq
     if os.path.exists(shotsDir):
         shots = os.listdir(shotsDir)
-        shotSel=hou.ui.selectFromList(shots,title='setShot')
-        project = os.path.join(shotsDir,shots[shotSel[0]])
-        print(project)
-        hou.hscript("set -g SHOT = {}".format(project))
-        print('set seq to:',hou.getenv('SHOTS'))
-        scene = os.path.join(shotsDir,shots[shotSel[0]],'fx','houdini')
-        hou.hscript("set -g SCENE = {}".format(scene))
+        if len(shots)!=0:
+            shotSel=hou.ui.selectFromList(shots,title='setShot')
+            shot = os.path.join(shotsDir,shots[shotSel[0]])
+            if(platform.system()=="Windows"):
+                shot = shot.replace("\\","/")
+            print(shot)
+            hou.hscript("set -g SHOT = {}".format(shot))
+            print('set seq to:',hou.getenv('SHOTS'))
+            scene = os.path.join(shotsDir,shots[shotSel[0]],'fx','houdini')
+            if(platform.system()=="Windows"):
+                scene = scene.replace("\\","/")
+                print('setting scene to :',scene)
+            hou.hscript("set -g SCENE = {}".format(scene))
+        else:
+            print('No shots found:',shots)
+            if hou.ui.displayConfirmation("Create folder?", suppress=hou.confirmType.OverwriteFile):
+                input_shotname = hou.ui.readInput('input shot name')
+                dir_path = os.path.join(shotsDir,input_shotname[1])
+                print(dir_path)
+                os.makedirs(dir_path,exist_ok=True)
+    else:
+        print('create seq folder please')
