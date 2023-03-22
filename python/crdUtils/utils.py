fb78fac5b843ac5d72cfeac3636195c822b910ec
Manishkumar Makhija <43664088+manx151089@users.noreply.github.com>
2023-03-22 16:11:44 +0000

    added some utils for crowd with comments

--- python/crdUtils/utils.py
+++ python/crdUtils/utils.py
@@ -3,13 +3,19 @@ import json
 
 
 def applyParm(node, name, value):
-    parm = node.parm(name)
-    parm.set(value)
-    parm.pressButton()
-    
+    """
+    This function takes in a node, a parameter name, and a value. It sets the value of the specified parameter 
+    for the node and presses the button to apply the new value.
+    """
+    parm = node.parm(name) # get the specified parameter
+    parm.set(value) # set the value of the parameter
+    parm.pressButton() # press the button to apply the new value
     
 def getStringParm(node, name):
-    return node.parm(name).eval().strip()
+    """
+    This function takes in a node and a parameter name. It returns the value of the specified parameter as a string.
+    """
+    return node.parm(name).eval().strip() # get the parameter value and return it as a string
     
     
 def buildMenuItems(items):
@@ -47,9 +53,16 @@ def refreshParmMenu(node, parmName, default = None):
 
 
 def mergeDicts(d, *other_dicts):
+    """
+    This will merge two dicts and even recursively merge the input children
+
+    Arguments:
+    - d: a dictionary
+    - *other_dicts: any number of addition dictionaries to merge
+    """
     result = copy.deepcopy(d)
     for other_dict in other_dicts:
-        for key, value in other_dict.iteritems():
+        for key, value in other_dict.items():
             if key in result and isinstance(result[key], dict) and isinstance(value, dict):
                 value = mergeDicts(result[key], value)
             else:
@@ -59,10 +72,20 @@ def mergeDicts(d, *other_dicts):
 
 
 def formatDictToStr(dataDict):
+    """
+    Converts a nested dictionary to a string with a specific format.
+
+    Arguments:
+    - dataDict: A nested dictionary to convert to a string.
+
+    Returns:
+    A string representation of the input dictionary, where each level of the nested dictionary is indented and
+    each key-value pair is separated by a comma. The format of the string is tailored to a specific use case.
+    """
     dataStr = ''
-    for keyL1, valuesL1 in sorted(dataDict.iteritems()):
+    for keyL1, valuesL1 in sorted(dataDict.items()):
         dataStr += '{}\n'.format(keyL1)
-        for keyL2, valuesL2 in sorted(valuesL1.iteritems()):
+        for keyL2, valuesL2 in sorted(valuesL1.items()):
             dataStr += '\t{}\n\t\t'.format(keyL2)
             count = 0
             for keyL3 in sorted(valuesL2.keys()):
