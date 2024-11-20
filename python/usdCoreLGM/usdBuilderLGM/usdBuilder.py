90f8821f2474e572e6637b948a759829c127f5c1
Manishkumar Makhija <43664088+manx151089@users.noreply.github.com>
2024-11-21 00:35:47 +0530

    adding working version of usd builder

--- python/usdCoreLGM/usdBuilderLGM/usdBuilder.py
+++ python/usdCoreLGM/usdBuilderLGM/usdBuilder.py
@@ -18,31 +18,40 @@ def load_usd_files_from_departments(shotname,show,pub_directory="usd"):
         show (str path): path of the show
     Returns:
         None
+
+    TO-DO:
+    Need to divide this function into two one to make a list of depts 
+    and one to load a single stage from the dept and append to the current stage
     '''
-    base_directory = os.path.join(show,pub_directory,shotname)
-    """
+    
+    ###NEED TO FIX THIS ONE NOW to use LSA properly and test in houdini LSA first and this script later
+    #departments = lsa.list_depts_from_shot
+    directory = pub_directory
+    base_directory = os.path.join(show,shot,directory)
     departments = [
         d for d in os.listdir(base_directory) 
         if os.path.isdir(os.path.join(base_directory, d))
     ]
-    """
-    ###NEED TO FIX THIS ONE NOW to use LSA properly and test in houdini LSA first and this script later
-    departments = lsa.list_depts_from_shot
+
     usd_files_to_load = []
     stage = hou.pwd().editableStage()
 
     for dept in departments:
-        usd_file_path = os.path.join(base_directory, dept, "geometry.usd")
-
+        usd_file_path = os.path.join(base_directory, dept, "geometry_v001.usd")
+        usd_file_path = os.path.normpath(usd_file_path)
         if os.path.exists(usd_file_path):
             usd_files_to_load.append(usd_file_path)
         else:
-            hou.ui.displayMessage(f"USD file not found in department '{dept}': {usd_file_path}")
+            #hou.ui.displayMessage(f"USD file not found in department '{dept}': {usd_file_path}")
+            print(f"USD file not found in department '{dept}': {usd_file_path}")
 
+    print('usd_files_to_load:',usd_files_to_load)
     for usd_file in usd_files_to_load:
         stage.GetRootLayer().subLayerPaths.append(usd_file)
 
     hou.ui.displayMessage(f"USD files loaded from departments: {', '.join(departments)}")
+    
+    
 
 
 if __name__ == "__main__":
