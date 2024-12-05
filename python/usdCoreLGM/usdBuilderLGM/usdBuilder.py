9e280576b3de6fdffc465844dcbf9206c9a86a95
Manishkumar Makhija <43664088+manx151089@users.noreply.github.com>
2024-12-05 15:22:52 +0530

    adding lgmfx usd builder and updating usdBuilder.py with scripts to be sourced inside the HDA's Python LOP

--- python/usdCoreLGM/usdBuilderLGM/usdBuilder.py
+++ python/usdCoreLGM/usdBuilderLGM/usdBuilder.py
@@ -35,51 +35,64 @@ def load_usd_files_from_departments(shotname,show,pub_directory="usd"):
     
     dept_paths,departments = lsa.lsa.list_depts_from_shot(shotname)    
     usd_files_to_load = [lsa.lsa.get_versions_from_dept(dept_path,list_all_versions=False) for dept_path in dept_paths]
-
-    #print('PRELOAD : usd files found:',usd_files_to_load)
     stage = hou.pwd().editableStage()
+    
     for usd_file in usd_files_to_load:
         if os.path.exists(usd_file):
             root = stage.GetEditTarget().GetLayer()
             layer = root.subLayerPaths.append(usd_file)
-            #print(f"USD file appended: {usd_file} \n in layer: {layer}")
-
         else:
             print(f"USD file not found: {usd_file}")
     
 def get_dict_with_versions(shotname,get_latest_version=False):
+    """
+    Uses the lsa (LGMFX Shot Api) to concatenate all the versions and returns
+    a list of departments and a dictionary of all versions.
+
+    Usage:
+    get_dict_with_versions(shotname,get_latest_version=False)
+
+    Args:
+    shotname (str. path) : the directory to the shot
+    get_latest_version (bool.) : If set to True this will return a list of latest versions
+      instead of a dictionary of all versions. Default is False.
+
+    Returns: 
+    [[departments] {int version_number:str version_path}] 
+    or
+    [[str departments] [str latest_versions]]]
+    
+    """
     dept_paths,departments = lsa.lsa.list_depts_from_shot(shotname)
     if get_latest_version:
-        latest_versions = [lsa.lsa.get_versions_from_dept(dept_path,list_all_versions=False) for dept_path in dept_paths]
+        latest_versions = [lsa.lsa.get_versions_from_dept(dept_path,list_all_versions=False) 
+                           for dept_path in dept_paths]
         return latest_versions
     else:
         all_versions = [lsa.lsa.get_versions_from_dept(dept_path) for dept_path in dept_paths]
-        #idx = 0
-        #dept_dict = {}
-        #for department in departments:
-            #dept_versions = all_versions[idx]
-            #print(department,dept_versions,'<<<<<<<<<<\n\n\n\n')
-            #dept_dict['tst']= dept_versions[idx]
-            #idx+=1
         return [departments,all_versions]
         
-        
-def set_multiparms_with_dict(node):
-    depts = node.parm('depts')
-    
 
 def make_dept_menu(node):
+    """
+    Uses the function get_dict_with_versions to return the data needed to 
+    create the menu.
+    """
     shot = hou.getenv('SHOT')
     versions = get_dict_with_versions(shot)
     return versions
     
 
 def buildb(node):
+    """
+    A callback script for the button 'build' which builds with
+    the selected versions in the UI.
+    """
     mp = node.parm('depts')
     mp_data = mp.multiParmInstancesAsData()
     build_data = []
     for mpi in mp_data:
-        if mpi['live_dept_path#'] is '':
+        if mpi['live_dept_path#'] == '':
             mpi['live_dept_path#']=mpi['dept_path#']
         if mpi['live_dept_path#'] is not mpi['dept_path#']:
             mpi['live_dept_path#']=mpi['dept_path#']
@@ -88,14 +101,73 @@ def buildb(node):
     
     
 def load(node):
+    """
+    This is a callback script for the button 'load'
+    It will let the user load new departments that do not exist in the 
+    multiparm 'depts'.
+    """
+    mp = node.parm('depts')
+    if mp.evalAsInt() == 0:
+        mp.set(1)
+        [x.set(x.menuItems()[0]) for x in mp.multiParmInstances() if 'dept_name' in x.name()]
+        [x.set(x.menuItems()[-1]) for x in mp.multiParmInstances() if 'dept_path' in x.name() and 'live_' not in x.name()]
     mp = node.parm('depts')
     mp_data = mp.multiParmInstancesAsData()
     depts_loaded = [x['dept_name#'] for x in mp_data]
     departments,versions = make_dept_menu(node)
-    #depts_loaded.remove(departments)
     [departments.remove(x) for x in depts_loaded]
     for dept in departments:
         dept_index= mp.evalAsInt()
-        mp.insertMultiParmInstance()
-        print(mp.multiParmInstancesAsData())
+        mp.insertMultiParmInstance(dept_index)
+        dept_index = mp.evalAsInt()
+        mp_data = mp.multiParmInstancesAsData()
+        mp_data[-1]['dept_name#']=dept
+        mp.setMultiParmInstancesFromData(mp_data)
+        dept_path = hou.parm('dept_path'+str(dept_index))
+        path = dept_path.menuItems()[-1]
+        dept_path.set(path)
+        
+
+def update(node):
+    """
+    Callback script for the button Updates versions to Latest.
+    It just make the currently selected versions in all the departments in the
+    multi-parm to be latest.
+    """
+    mp = node.parm('depts')
+    depts_paths = [x for x in mp.multiParmInstances() if 'dept_path' in 
+                   x.name() and 'live_' not in x.name()]
+    latest_versions = [x.set(x.menuItems()[-1]) for x in 
+                       mp.multiParmInstances() if 'dept_path' in 
+                       x.name() and 'live_' not in x.name()]
     
+def append_stage_with_paths(path_list=[]):
+    """
+    Use in a python LOP to build the stage with a list of paths.
+    
+    Args:
+        path_list (str. path list) : list of paths to `.usd` files.
+    """
+    if len(path_list) != 0:
+        stage = hou.pwd().editableStage()
+        usd_files_to_load = path_list
+        for usd_file in usd_files_to_load:
+            if os.path.exists(usd_file):
+                root = stage.GetEditTarget().GetLayer()
+                layer = root.subLayerPaths.append(usd_file)
+            else:
+                print(f"USD file not found: {usd_file}")
+
+def get_usd_path_list(node):
+    """
+    This will return a list of values for multiParmInstances 
+    of Current Dept Path.
+
+        Args:
+            node(hou.Node): Usually the multiparm would be on a parent hda. 
+            Use `hou.pwd().parent()` to get to the node.
+    """
+    mp = node.parm('depts')
+    path_list = [x.eval() for x in mp.multiParmInstances()
+                  if 'live_dept_path' in x.name()]
+    return path_list
